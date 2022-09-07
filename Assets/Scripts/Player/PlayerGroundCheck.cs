d0e0ae9555dec62526c4b5cb4dda178f7517f39b
Chowdog <yangerplato3465@gmail.com>
2022-09-07 22:41:22 +0800

    Sync players between clients, player movements

---
+++ Assets/Scripts/Player/PlayerGroundCheck.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour {
+    PlayerController playerController;
+    private void Awake() {
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject == playerController.gameObject) return;
+        playerController.SetGroundedState(true);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject == playerController.gameObject) return;
+        playerController.SetGroundedState(false);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(other.gameObject == playerController.gameObject) return;
+        playerController.SetGroundedState(true);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+         if(other.gameObject == playerController.gameObject) return;
+        playerController.SetGroundedState(true);
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if(other.gameObject == playerController.gameObject) return;
+        playerController.SetGroundedState(false);
+    }
+
+    private void OnCollisionStay(Collision other) {
+        if(other.gameObject == playerController.gameObject) return;
+        playerController.SetGroundedState(true);
+    }
+}
